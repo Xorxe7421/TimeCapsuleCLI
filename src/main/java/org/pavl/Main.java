eb13a0efc5a242f5d97f6797f0bfa4ae632e148d
Xorxe7421 <pavliashviligiorgi7421@gmail.com>
2025-12-12 18:46:42 +0400

    Simple jdk 25 change

--- src/main/java/org/pavl/Main.java
+++ src/main/java/org/pavl/Main.java
@@ -3,7 +3,7 @@ package org.pavl;
 import org.pavl.cli.CliHandler;
 
 public class Main {
-    public static void main(String[] args) {
+    static void main(String[] args) {
         CliHandler cliHandler = new CliHandler();
         cliHandler.handle(args);
     }
